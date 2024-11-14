39583af6fbe95a9a2c835d683cc46993fae2cdb2
asx-dev <asxdev18@gmail.com>
2024-11-14 11:48:45 +0000

    Phonebook backend step 1

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log("Server is running");
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+// Get contacts
+app.get("/api/persons", (req, res) => {
+  const contacts = [
+    {
+      id: "1",
+      name: "Arto Hellas",
+      number: "040-123456",
+    },
+    {
+      id: "2",
+      name: "Ada Lovelace",
+      number: "39-44-5323523",
+    },
+    {
+      id: "3",
+      name: "Dan Abramov",
+      number: "12-43-234345",
+    },
+    {
+      id: "4",
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+    },
+  ];
+  res.status(200).json(contacts);
+});
+
+// Server running
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
