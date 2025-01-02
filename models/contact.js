5142d5479659912d7b0dcd5ce4464ca172d8c8b0
asx-dev <asxdev18@gmail.com>
2025-01-02 14:29:49 +0000

    Contact Databases model formatted

--- models/contact.js
+++ models/contact.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 // Schema
-const contactSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  number: { type: String, required: true },
-});
+const contactSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    number: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 // Model
 const Contact = mongoose.model("Contact", contactSchema);
